b9465ebe7c55e0653bd9e42274252c7d8f68d186
Samiahaj <hajjisamia10@gmail.com>
2026-05-09 14:47:16 +0200

    news controller aanmaken, news routes toevoegen, Admin CRUD beveiligen

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('news', NewsController::class)
+    ->middleware(['auth', 'admin']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
