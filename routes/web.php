af96a4bd07ff71bb7f7873cbcadbcb25e3d38294
Samiahaj <hajjisamia10@gmail.com>
2026-05-10 17:57:44 +0200

    news routes wijzigen: news en news details zichtbaar voor public en news aanmaken, wijzigen en verwijderen voor admin

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('news', NewsController::class)
-    ->middleware(['auth', 'admin']);
+
+
+
+Route::get('/news', [NewsController::class, 'index'])
+    ->name('news.index');
+
+Route::get('/news/{news}', [NewsController::class, 'show'])
+    ->name('news.show');
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    Route::get('/admin/news/create', [NewsController::class, 'create'])
+        ->name('news.create');
+
+    Route::post('/admin/news', [NewsController::class, 'store'])
+        ->name('news.store');
+
+    Route::get('/admin/news/{news}/edit', [NewsController::class, 'edit'])
+        ->name('news.edit');
+
+    Route::put('/admin/news/{news}', [NewsController::class, 'update'])
+        ->name('news.update');
+
+    Route::delete('/admin/news/{news}', [NewsController::class, 'destroy'])
+        ->name('news.destroy');
+});
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
