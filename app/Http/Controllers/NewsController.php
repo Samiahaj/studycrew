c70d67aa7343034ae8811a85d87ecc7576601faf
Samiahaj <hajjisamia10@gmail.com>
2026-05-10 21:38:38 +0200

    method show

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -50,10 +50,10 @@ class NewsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+    public function show(News $news)
+{
+    return view('news.show', compact('news'));
+}
 
     /**
      * Show the form for editing the specified resource.
