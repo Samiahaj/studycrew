b0307fcff649e8d88a7fc6d9829b8e644eea4177
Samiahaj <hajjisamia10@gmail.com>
2026-05-11 16:34:10 +0200

    probleem opgelost in functie show

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -35,38 +35,40 @@ class NewsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-       $validated = $request->validate([
+   public function store(Request $request)
+{
+    $validated = $request->validate([
         'title' => 'required|max:255',
         'content' => 'required',
-          'image' => 'nullable|image|max:2048',
+        'image' => 'nullable|image|max:2048',
     ]);
 
     $imagePath = null;
 
-if ($request->hasFile('image')) {
+    if ($request->hasFile('image')) {
 
-    $imagePath = $request->file('image')
-        ->store('news-images', 'public');
-}
+        $imagePath = $request->file('image')
+            ->store('news-images', 'public');
+    }
+
     $news = News::create([
-        
- if($request->has('tags')) {
 
-    $news->tags()->attach($request->tags);
-}
         'user_id' => auth()->id(),
         'title' => $validated['title'],
         'content' => $validated['content'],
-         'image' => $imagePath,
+        'image' => $imagePath,
         'published_at' => now(),
     ]);
 
-    return redirect()
-    ->route('news.index')
-    ->with('success', 'Nieuwsartikel succesvol aangemaakt.');
+    if($request->has('tags')) {
+
+        $news->tags()->attach($request->tags);
     }
+
+    return redirect()
+        ->route('news.index')
+        ->with('success', 'Nieuwsartikel succesvol aangemaakt.');
+}
 
     /**
      * Display the specified resource.
