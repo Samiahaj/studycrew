2d05eb6a08f8635a38fc6f0476643f09b95fe124
Samiahaj <hajjisamia10@gmail.com>
2026-05-11 16:22:18 +0200

    tags chackboxes maken

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -43,6 +43,35 @@
 
     </div>
 
+
+<div>
+
+    <label class="font-bold">
+        Tags
+    </label>
+
+    <div class="space-y-2 mt-2">
+
+        @foreach($tags as $tag)
+
+            <div>
+
+                <input type="checkbox"
+                       name="tags[]"
+                       value="{{ $tag->id }}">
+
+                {{ $tag->name }}
+
+            </div>
+
+        @endforeach
+
+    </div>
+
+</div>
+
+
+
     <button class="bg-blue-500 text-white px-4 py-2 rounded">
 
         Aanmaken
