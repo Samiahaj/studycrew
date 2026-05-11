6bc4b76efcb3ce2fe6c7fbc5dcbf615f21a82f24
Samiahaj <hajjisamia10@gmail.com>
2026-05-11 20:55:02 +0200

    FAQ index methode

--- app/Http/Controllers/FAQController.php
+++ app/Http/Controllers/FAQController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\FAQCategory;
 
 class FAQController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = FAQCategory::with('faqs')->get();
+
+        return view('faq.index', compact('categories'));
+    }
 }
