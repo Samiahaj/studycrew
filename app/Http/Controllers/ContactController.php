d0ab7426d894751d32e4f9621d5ee588e8ec8a11
Samiahaj <hajjisamia10@gmail.com>
2026-05-15 18:55:52 +0200

    mail sturen aan admin

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMessageMail;
 
 class ContactController extends Controller
 {
@@ -11,15 +13,18 @@ class ContactController extends Controller
         return view('contact.index');
     }
 
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|max:255',
-            'email' => 'required|email',
-            'message' => 'required|max:1000',
-        ]);
+   public function store(Request $request)
+{
+    $request->validate([
+        'name' => 'required|max:255',
+        'email' => 'required|email',
+        'message' => 'required|max:1000',
+    ]);
 
-        return back()
-            ->with('success', 'Bericht succesvol verzonden.');
-    }
+    Mail::to('admin@ehb.be')
+        ->send(new ContactMessageMail($request->all()));
+
+    return back()
+        ->with('success', 'Bericht succesvol verzonden.');
+}
 }
---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,19 @@
+<h1>Nieuw contactbericht</h1>
+
+<p>
+    <strong>Naam:</strong>
+    {{ $data['name'] }}
+</p>
+
+<p>
+    <strong>Email:</strong>
+    {{ $data['email'] }}
+</p>
+
+<p>
+    <strong>Bericht:</strong>
+</p>
+
+<p>
+    {{ $data['message'] }}
+</p>
